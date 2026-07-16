f4b77606fc048d815e4b6a037749512fce264157
bijan453 <imatshoevbijan@gmail.com>
2026-07-16 14:50:15 +0500

    Auto-open browser when running server

--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ cd /d "%~dp0"
 echo Starting F-Mine Backend Server...
 echo Working directory: %CD%
 echo.
+start "" http://localhost:3000/lobby.html
 node server.js
 if %ERRORLEVEL% neq 0 (
   echo.
